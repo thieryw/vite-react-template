5c8e6fc5a213a8fed0a902d89a82dc82a0f18b18
thieryw <39378411+thieryw@users.noreply.github.com>
2023-11-01 20:59:33 +0000

    first personalistation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { App } from './App.tsx'
 import './index.css'
+import { RouteProvider } from "./router";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouteProvider>
+      <App />
+    </RouteProvider>
   </React.StrictMode>,
 )
